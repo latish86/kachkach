6e2c10dba21f0a8378603c71f1db2dd60fc9772f
latish86 <latish86@bk.ru>
2017-11-29 14:20:39 +0700

    Подключение normalize.css. Создание файла с переменными SCSS.

---
+++ front/src/components/app_header.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="app-header">
+    <div class="wrapper">
+      <div class="logo">kachkach</div>
+      <div class="login">login/registration</div>
+    </div>
+  </div>  
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  @import '../scss/variables.scss';
+
+  .app-header{
+    background-color: $dark-red;
+    color: $milk;
+    .wrapper{
+      max-width: 1200px;   
+      padding: 15px;   
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      .logo{
+        font-size: 30px;
+      }
+    }
+  }
+</style>
