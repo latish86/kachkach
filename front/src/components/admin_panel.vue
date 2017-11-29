27017a34476cc204b0113d51fb5e9aca5ad9ed95
latish86 <latish86@bk.ru>
2017-11-29 23:01:58 +0700

    Начало работы над шаблоном амин панели.

--- front/src/components/admin_panel.vue
+++ front/src/components/admin_panel.vue
@@ -1,20 +1,51 @@
 <template>
-  <h2>Admin Panel:</h2>
+  <div class="admin-panel">
+    <h2>Admin Panel:</h2>
+    <ul>
+      <li>Настройки</li>
+      <li>Добавить тестовые данные</li>
+      <li>Отчистить БД</li>
+      <li>Пользователи</li>
+      <li>Разное</li>
+    </ul>
+    <component v-bind:is="currentView"></component>
+  </div>
+  
 </template>
 
 <script>
+import ChangeData from './change-data.vue'
+
 export default {
+  components:{
+    'change-data': ChangeData
+  },
   data(){
     return{
-
+      currentView: 'change-data'
     }
   }
 }
 </script>
 
 <style lang='scss' scoped>
+  @import '../scss/variables.scss';
+
   h2{
     text-align: center;
+  }
+  ul{
+    padding: 0;
+    display: flex;
+    justify-content: center;
+    li{
+      list-style-type: none;
+      margin-right: 15px;
+      &:hover{
+        color: $dark-red;
+        cursor: pointer;
+      }
+    }
   }
 </style>
 
---
+++ front/src/components/change-data.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="change-data">
+    <h3 style="color: #24c2cb;">Добавить всплывающие сообщения(ошибки, удачки и т.п)</h3>
+    Изменить данные в базе
+    <h2>Добавить запись в базу</h2>
+    <label>День: </label><input type="text">
+    <label>Месяц: </label><input type="text">
+    <label>Год: </label><input type="text">
+    <br><br>
+    <label>Подходы(числа через запятую): </label><input type="text"><div class="button">Добавить</div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
+
+
